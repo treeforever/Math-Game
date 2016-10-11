97dbad0466f4db978156f18672cb61c95257c1f2
treeforever <treeforever7@gmail.com>
2016-10-11 19:42:00 -0400

    in working process

--- main.rb
+++ main.rb
@@ -9,16 +9,13 @@ require "./Turn"
 class Game
   def initialize(num_of_player)
     @num = num_of_player
-    @players = []
 
-    for j in 0..1
-     @players[j] = Player.new('3/3')
-    end
+    @players = @num.times.map { |i| Player.new }
+    @current_player_index
   end
 
-  def decide_turn(i, num_of_player)
-    turn = Turn.new(i, num_of_player)
-    player_num = turn.decide_turn
+  def next_turn
+    @current_player_index = (@current_player_index + 1) % @size
   end
 
   def ask_question(player_num)
@@ -30,6 +27,17 @@ class Game
     if_correct = (user_input == q_and_a[1])
     @players[player_num - 1].score = @players[player_num - 1].score_eveluate(@players[player_num - 1].score, if_correct)
     if_correct
+  end
+
+  # def customized_scores(players)
+  #   if players.length
+  #   players.each { |p| puts }
+
+  def alive? (player)
+    if player.score == Rational('0/1')
+      puts "Player ?? wins with a score of #{@players[??].score}"
+      puts "------------- Game Over --------------"
+      exit
   end
 
   def game_feedback(if_correct, player_num)
@@ -52,16 +60,17 @@ class Game
   end
 
   def start_game
-    i = 0
-    while(@players[0].score.to_r > 0 && @players[1].score.to_r > 0)
+    while(@players.count { |p| p.score > 0} > 1)
       which_player = decide_turn(i, @num)
       boolean_value = ask_question(which_player)
       game_feedback(boolean_value, which_player)
       i += 1
     end
+
+    
   end
 
 end
 
-game = Game.new(2)
+game = Game.new(3)
 game.start_game
